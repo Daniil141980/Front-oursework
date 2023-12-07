bb364fdeac30e5d200c8aaa36b7de6fac059362e
Daniil <Daniil141980@yandex.ru>
2023-12-07 20:25:51 +0300

    all ready 3.0

--- scripts/basket.js
+++ scripts/basket.js
@@ -20,7 +20,7 @@ function update() {
                                 <div id="btn5"><button onclick="decrementCount(${product.id})">-</button></div>
                             </div>
                             <div class="shopping-cart-total-price-item">
-                                <h4>Cost:</h4>
+                                <h4>Стоимость:</h4>
                                 <h4 class="shopping-cart-item-price">${product.quantity * product.price} ₽</h4>
                             </div>
                         </div>
@@ -29,7 +29,7 @@ function update() {
 
     if (totalCost !== 0) {
         out += `<div class="cart-total-cost">
-                <h4>Total:<br/>${totalCost} ₽</h4>
+                <h4>Итог:<br/>${totalCost} ₽</h4>
             </div>`
     }
 
@@ -85,7 +85,7 @@ fetch("./data/products.json")
                                 <div id="btn5"><button onclick="decrementCount(${product.id})">-</button></div>
                             </div>
                             <div class="shopping-cart-total-price-item">
-                                <h4>Cost:</h4>
+                                <h4>Стоимость:</h4>
                                 <h4 class="shopping-cart-item-price">${product.quantity * product.price} ₽</h4>
                             </div>
                         </div>
@@ -94,7 +94,7 @@ fetch("./data/products.json")
 
         if (totalCost !== 0) {
             out += `<div class="cart-total-cost">
-                <h4>Total:<br/>${totalCost} ₽</h4>
+                <h4>Итог:<br/>${totalCost} ₽</h4>
             </div>`
         }
 
